33d177f9d6d309db68492d1e771c1c99a63e5c1b
Liam <liam@cpan.org>
2016-11-24 15:02:07 -0600

    [zsh] emacs key bindings, magic-space

--- .zsh/behavior.zsh
+++ .zsh/behavior.zsh
@@ -1,6 +1,11 @@
+bindkey -e # use emacs key bindings
+
+# up/down fuzzy history finding
 autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search
 bindkey "^[[B" down-line-or-beginning-search
+
+bindkey ' ' magic-space
