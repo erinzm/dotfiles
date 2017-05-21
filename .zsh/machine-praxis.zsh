8d58e1b1036de364fd4c517110837bea7be8e49d
Liam <liam@cpan.org>
2017-05-21 17:02:17 -0500

    praxis-specific Ruby config stuff

--- .zsh/machine-praxis.zsh
+++ .zsh/machine-praxis.zsh
@@ -4,11 +4,16 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/cuda/lib64:/opt/cuda/extras/CUPTI/
 export CUDA_HOME=/opt/cuda
 export PYTHONPATH=$HOME/build/caffe-rc4/python:$PYTHONPATH
 
+# RubyGems configuration
+export GEM_HOME=$(ruby -rubygems -e "puts Gem.user_dir")
+
 path+=(
 	$HOME/.cargo/bin
-	$(ruby -rubygems -e "puts Gem.user_dir")/bin
+	$GEM_HOME/bin
 	$path)
 
 # OPAM configuration
 . /home/liam/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
 
+# added by travis gem
+[ -f /home/liam/.travis/travis.sh ] && source /home/liam/.travis/travis.sh
