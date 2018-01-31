2ffce1f8e72f474384a864d8848835ea2dc74889
Liam <liam@cpan.org>
2018-01-31 10:51:48 -0600

    machine-praxis: fix various path issues

--- .zsh/machine-praxis.zsh
+++ .zsh/machine-praxis.zsh
@@ -1,19 +1,17 @@
 typeset -U path
 
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/cuda/lib64:/opt/cuda/extras/CUPTI/lib64"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/cuda/lib64:/opt/cuda/extras/CUPTI/lib64:"
 export CUDA_HOME=/opt/cuda
-export PYTHONPATH=$HOME/build/caffe-rc4/python:$PYTHONPATH
+export PYTHONPATH=$HOME/software/rdkit:$HOME/build/caffe-rc4/python:$PYTHONPATH
 
 # RubyGems configuration
-export GEM_HOME=$(ruby -rubygems -e "puts Gem.user_dir")
+export GEM_HOME=$(ruby -rrubygems -e "puts Gem.user_dir")
 
 path+=(
 	$HOME/.cargo/bin
 	$GEM_HOME/bin
+	$HOME/.config/yarn/global/node_modules/.bin/
 	$path)
-
-# OPAM configuration
-. /home/liam/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
 
 # added by travis gem
 [ -f /home/liam/.travis/travis.sh ] && source /home/liam/.travis/travis.sh
